65585b25b0b25fdcc0788b5e613d26f8df8333d6
Marek Sedlak <bodka.zavinac@gmail.com>
2018-11-02 21:48:58 +0100

    another update

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 install_netcat()
 {
-	echo "Installing netcat..."
+	echo "Installing netcat (opkg install netcat) ..."
     opkg -V0 update
     opkg -V0 install netcat
 }
@@ -13,10 +13,11 @@ finish(){
     echo "NICE ;)"
     echo "There are 2 important steps to finish this setup:"
     echo ""
-    echo "1. Make sure this line is in the cron. To do this, run: crontab -e"
+    echo "1. Make sure this line is in the cron. To open the cron editor, run: crontab -e"
     echo "   */2 * * * * $DIR/internet-keep-alive.sh"
     echo ""  
-    echo "2. The interface representing the LTE connection is set to 'wwan'. To edit this, edit the 'restart-interface.sh'."
+    echo "2. The interface representing the LTE connection is set to 'wwan'."
+    echo "   To change the interface, please edit the line INTERFACE='wwan' in the 'restart-interface.sh'."
     echo ""
     echo "Enjoy!"
 
@@ -26,7 +27,7 @@ finish(){
 download_files()
 {
 	DIR=$( cd $(dirname $0) ; pwd -P )
-	echo "Downloading files..."
+	echo "Downloading files from https://github.com/mchsk/openwrt-lte-keep-alive ..."
     wget -q --no-check-certificate https://raw.githubusercontent.com/mchsk/openwrt-lte-keep-alive/master/dns-test.sh -O dns-test.sh && chmod +x dns-test.sh
     wget -q --no-check-certificate https://raw.githubusercontent.com/mchsk/openwrt-lte-keep-alive/master/internet-keep-alive.sh -O internet-keep-alive.sh && chmod +x internet-keep-alive.sh
     wget -q --no-check-certificate https://raw.githubusercontent.com/mchsk/openwrt-lte-keep-alive/master/restart-interface.sh -O restart-interface.sh && chmod +x restart-interface.sh
