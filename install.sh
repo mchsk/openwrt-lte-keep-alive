de9e4813015ac17e53af0dddcb017836dbf1c414
Marek Sedlak <bodka.zavinac@gmail.com>
2018-11-02 21:18:36 +0100

    installation script

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/ash
+# Installation script.
+
+install_netcat()
+{
+	echo "Installing netcat..."
+    opkg -V0 update
+    opkg -V0 install netcat
+}
+
+download_files()
+{
+	DIR=$( cd $(dirname $0) ; pwd -P )
+	echo "Downloading files..."
+    wget -q --no-check-certificate https://raw.githubusercontent.com/mchsk/openwrt-lte-keep-alive/master/dns-test.sh -O dns-test.sh && chmod +x dns-test.sh
+    wget -q --no-check-certificate https://raw.githubusercontent.com/mchsk/openwrt-lte-keep-alive/master/internet-keep-alive.sh -O internet-keep-alive.sh && chmod +x internet-keep-alive.sh
+    wget -q --no-check-certificate https://raw.githubusercontent.com/mchsk/openwrt-lte-keep-alive/master/restart-interface.sh -O restart-interface.sh && chmod +x restart-interface.sh
+    wget -q --no-check-certificate https://raw.githubusercontent.com/mchsk/openwrt-lte-keep-alive/master/restart-router.sh -O restart-router.sh && chmod +x restart-router.sh
+    echo ""
+    echo "NICE ;)"
+    echo ""
+    echo "1. Make sure this line is in the cron:"
+    echo "   */2 * * * * $DIR/internet-keep-alive.sh"
+    echo "   To do this, run: crontab -e"
+    echo "2. The interface representing the LTE connection is set to 'wwan'. To edit this, edit the 'restart-interface.sh'."
+    echo "3. Enjoy!"
+
+    rm install.sh
+}
+
+echo ""
+echo "OpenWRT Keep-alive scripts."
+
+while true; do
+    read -p "This will install netcat as a prerequisite. Do you want to continue (y/n)?: " yn
+    case $yn in
+        [Yy]* ) install_netcat; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer 'y' or 'n'.";;
+    esac
+done
+
+echo ""
+DIR=$( cd $(dirname $0) ; pwd -P )
+while true; do
+    read -p "This will download the files into $DIR. Do you want to continue (y/n)?: " yn
+    case $yn in
+        [Yy]* ) download_files; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer 'y' or 'n'.";;
+    esac
+done
+
+
+
